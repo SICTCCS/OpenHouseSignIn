49bc1293685620085083d2a75d5c60dbfa5dc357
sushi2006 <alapatisu@gmail.com>
2024-09-05 14:01:20 -0500

    made magic box

--- Main Form/script.js
+++ Main Form/script.js
@@ -1,29 +1,5 @@
-const selectBtn = document.querySelector(".select-btn"),
-      items = document.querySelectorAll(".item");
-
-selectBtn.addEventListener("click", () => {
-    selectBtn.classList.toggle("open");
-});
-
-items.forEach(item => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("checked");
-        console.log(item.classList);
-        let checked = document.querySelectorAll(".checked")
-        let c = document.getElementById("cb1check");
-        console.log(c.toggle("checked"))
-    
-            btnText = document.querySelector(".btn-text");
-
-            if(checked && checked.length > 0){
-                btnText.innerText = `${checked.length} Selected`;
-            }else{
-                btnText.innerText = "Select Language";
-            }
-
-            if(checked){
-                console.log(item.classList.toggle("checked"));
-                console.log(document.getElementById("cb1TXT").innerHTML)
-            }
-    });
+let checkbox = document.getElementById("other");
+checkbox.addEventListener("change", () => {
+    let textBox = document.getElementById("otherReference")
+    textBox.style = checkbox.checked ? "display:block" : "display:none";
 })
