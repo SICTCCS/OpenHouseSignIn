af6cf6abcf0f896a9e8f8acb28c2c10b46d1faa9
Claire Spindler <claire.spindler@stu.evsck12.com>
2024-09-19 13:51:14 -0500

    Edited the HTML to send to current sheets and fixed some CSS

--- Main Form/script.js
+++ Main Form/script.js
@@ -3,3 +3,24 @@ checkbox.addEventListener("change", () => {
     let textBox = document.getElementById("otherReference")
     textBox.style = checkbox.checked ? "display:block" : "display:none";
 })
+
+
+// Code from https://codepen.io/levinunnink-the-bashful/pen/YzxPyoG?editors=0010
+
+// edit the custom notification
+
+window.addEventListener("load", function() {
+    const form = document.getElementById('submit-to-google-sheet');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Success!");
+      })
+    });
+  });
