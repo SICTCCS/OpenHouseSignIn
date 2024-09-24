668c6c594986d6c91f28e02afd9aa1f80b339ecd
B2 <claire.spindler@stu.evsck12.com>
2024-09-24 14:05:22 -0500

    edited the form so it doesn't redirect and alerts

--- Main Form/script.js
+++ Main Form/script.js
@@ -9,18 +9,10 @@ checkbox.addEventListener("change", () => {
 
 //* edit the custom notification *//
 
-window.addEventListener("load", function() {
-    const form = document.getElementById('submit-to-google-sheet');
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: 'POST',
-        body: data,
-      })
-      .then(() => {
-        alert("Success!");
-      })
-    });
-  });
+const form = document.getElementById('submit-to-google-sheet');
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const data = new FormData(form);
+  const action = e.target.action;
+    alert("Successfully Submitted!");
+});
