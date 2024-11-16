c31f5db3b8a6dbd9a330fbad4301d88ddd95fbf4
clairespin01 <claire.spindler@stu.evsck12.com>
2024-11-16 10:32:43 -0600

    updated javascript

--- sub.js
+++ sub.js
@@ -1,15 +1,17 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycby6Qktc7J3uxIPCdzJ5nTdcdITaFF2XVpcc3Gtaq1ZEb6BiZHNnvLtY6aFeHRctyO1I/exec';
 const form = document.forms['submit-to-google-sheet']
-const statusMessage = document.getElementById("")
+const statusMessage = document.getElementById("statusMessage")
 
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+  const data = new FormData(form);
+  const action = e.target.action;
   statusMessage.innerText = "Please Wait...";
   statusMessage.style.color = "black";
   statusMessage.style.display = "block";
 
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  fetch(action, { method: 'POST', body: data ,})
     .then(response => {
         statusMessage.innerText = "Success!";
         statusMessage.style.color = "#45a049";
