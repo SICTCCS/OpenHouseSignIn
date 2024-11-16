fe2e64c908ebe6d2be9b5fee6af11d0ef2d67b81
clairespin01 <claire.spindler@stu.evsck12.com>
2024-11-16 10:38:28 -0600

    updated the java to match main form

--- sub.js
+++ sub.js
@@ -1,27 +1,73 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycby6Qktc7J3uxIPCdzJ5nTdcdITaFF2XVpcc3Gtaq1ZEb6BiZHNnvLtY6aFeHRctyO1I/exec';
-const form = document.forms['submit-to-google-sheet']
+const form = document.getElementById('submit-to-google-sheet');
 const statusMessage = document.getElementById("statusMessage")
 
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   const data = new FormData(form);
+//   const action = e.target.action;
+//   statusMessage.innerText = "Please Wait...";
+//   statusMessage.style.color = "black";
+//   statusMessage.style.display = "block";
+
+//   fetch(action, { method: 'POST', body: data ,})
+//     .then(response => {
+//         statusMessage.innerText = "Success!";
+//         statusMessage.style.color = "#45a049";
+//     })
+//     .catch(error => {
+//         statusMessage.innerText = "Error, try again.";
+//         statusMessage.style.color = "#a04545";
+//     })
+//     .finally(() => {
+//         setTimeout(() => statusMessage.style.display = "none", 3000);
+//     })
+//   e.currentTarget.reset()
+// })
+
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
   const data = new FormData(form);
   const action = e.target.action;
-  statusMessage.innerText = "Please Wait...";
-  statusMessage.style.color = "black";
-  statusMessage.style.display = "block";
 
-  fetch(action, { method: 'POST', body: data ,})
-    .then(response => {
-        statusMessage.innerText = "Success!";
-        statusMessage.style.color = "#45a049";
+    fetch(action, {
+      method: 'POST',
+      body: data,
+      
     })
-    .catch(error => {
-        statusMessage.innerText = "Error, try again.";
+    .then(() => {
+      form.reset();
+      regBTN.disabled=false;
+	    otherReference.style = "display:none";
+      statusMessage.innerText = "Success!";
+      statusMessage.style.color = "#45a049";
+    })
+    .catch((error) => {
+      alert("Submission Failed, please try again.\n"+error);
+      statusMessage.innerText = "Error, try again.";
         statusMessage.style.color = "#a04545";
+	    otherReference.style = "display:none";
     })
-    .finally(() => {
-        setTimeout(() => statusMessage.style.display = "none", 3000);
-    })
-  e.currentTarget.reset()
-})
+
+    let name = firstname.value;
+    name += " " + lastname.value;
+
+    let school = document.getElementById("schoolSelect").value;
+
+	// userIDCounter++;
+	// if (userIDCounter > 255) {
+		// userIDCounter = 0;
+		// kioskID = (parseInt(kioskID, 16) + 3).toString(16).toUpperCase();
+	// }
+
+  // let id = kioskID + userIDCounter.toString(16).padStart(2, "0").toUpperCase();
+
+	// userID.value = "'" + id;
+	// console.log(userID.value);
+
+  // print_label(id, name, school);
+
+});
+document.getElementById('RegisterBTN').disabled=false;
